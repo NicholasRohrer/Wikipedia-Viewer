5b9148e6346bd455a3418543292ba48dae4c9ddf
Nicholas Rohrer <nrohrer2@gmail.com>
2016-10-30 16:31:03 -0500

    added magnifying glass toggle for search bar

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
 $(document).ready();
 
+//cache the DOM
+var $searchIcon = $('#searchIcon');
+var $searchInput = $('#searchInput');
+var $searchClose = $('#searchClose');
+
+
+
+
+// toggle search bar for user
+function toggleSearch() {
+	$searchInput.slideToggle(300, removeText);
+}
+
+// delete any text user has input
+function removeText() {
+	$searchInput.val('');
+}
+
+// click magnifying glass to hide and bring up search input
+$searchIcon.on("click", toggleSearch);
+
